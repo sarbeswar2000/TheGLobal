9b2d47300afd51073a96bab3cb31e5234c5fb4c1
sarbeswar2000 <sarbeswar58behera@gmail.com>
2023-07-04 19:35:29 -0700

    Initial commit

---
+++ src/components/NewsItem.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    let { title, description, url, urlToImage } = this.props;
+    return (
+      <>
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src={
+              !urlToImage
+                ? "https://images.moneycontrol.com/static-mcnews/2020/11/crude-oiloil-770x433.jpg"
+                : urlToImage
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a href={url} className="btn btn-dark">
+              ClicktoNewPage
+            </a>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default NewsItem;
